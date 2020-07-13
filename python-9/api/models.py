d78366fc547f2b64bc410a878f62eb78cef2f6f6
Gustavo Cainã Branco <cainabranco@gmail.com>
2020-07-13 17:28:39 -0300

    python-9 final commit

--- python-9/api/models.py
+++ python-9/api/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.core.validators import EmailValidator, MinLengthValidator, validate_ipv4_address
+from django.core.exceptions import ValidationError
+
+
+def level_validator(level):
+    if level not in ['CRITICAL', 'DEBUG', 'ERROR', 'WARNING', 'INFO']:
+        raise ValidationError('Level not allowed')
 
 
 # Create your models here.
@@ -6,8 +13,11 @@ from django.db import models
 class User(models.Model):
     name = models.CharField('Nome', max_length=50)
     last_login = models.DateTimeField('Último Login', auto_now=True)
-    email = models.EmailField('E-mail', max_length=254)
-    password = models.CharField('Senha', max_length=50)
+    email = models.EmailField('E-mail', max_length=254, validators=[EmailValidator])
+    password = models.CharField('Senha', max_length=50, validators=[MinLengthValidator(8)])
+
+    def __str__(self):
+        return self.name
 
 
 class Agent(models.Model):
@@ -15,20 +25,29 @@ class Agent(models.Model):
     status = models.BooleanField('Status')
     env = models.CharField('Env', max_length=20)
     version = models.CharField('Versão', max_length=5)
-    address = models.GenericIPAddressField('Endereço', max_length=39)
+    address = models.GenericIPAddressField('Endereço', max_length=39, validators=[validate_ipv4_address])
+
+    def __str__(self):
+        return self.name
 
 
 class Event(models.Model):
-    level = models.CharField('Nível', max_length=20)
+    level = models.CharField('Nível', max_length=20, validators=[level_validator])
     data = models.TextField('Dado')
     arquivado = models.BooleanField('Arquivado')
     date = models.DateField('Data', auto_now=True)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.level
+
 
 class Group(models.Model):
     name = models.CharField('Nome', max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class GroupUser(models.Model):
